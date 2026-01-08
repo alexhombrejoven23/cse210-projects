cc40e66b680f2a2524d93dd83df9090285ff10ef
Alex <alexcondorimolle2025@gmail.com>
2026-01-08 16:52:21 -0400

    first commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello World! My name is Alex Condori from Bolivia, I really want to learn to build software I like creating many new things. Sometimes I wonder about humanity's problems and want to help, but I know that to do this I need to contribute something that solves their problems so they can be happy.");
     }
 }
