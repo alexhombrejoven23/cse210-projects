64514ef6bcab32621d2bd076a2ec13de7d44bf7a
Alex <alexcondorimolle2025@gmail.com>
2026-01-09 23:28:17 -0400

    Commit exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+         Random rnd = new Random();          
+        string playAgain;                  
+
+        do  
+        {
+            int magic = rnd.Next(1, 101);   
+            int guess;                      
+            int tries = 0;                
+
+            Console.WriteLine("Guess My Number!");
+    
+            do  
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                tries++;
+
+                if (guess < magic)      Console.WriteLine("Higher");
+                else if (guess > magic) Console.WriteLine("Lower");
+                else                    Console.WriteLine("You guessed it!");
+            } while (guess != magic);
+
+            Console.WriteLine($"You made {tries} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
