433203698c47497cbd07687dde609bd31445c93b
Alex <alexcondorimolle2025@gmail.com>
2026-01-09 23:56:51 -0400

    commit exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+
+        int squared = SquareNumber(number);
+
+        DisplayResult(name, squared);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int n)
+    {
+        return n * n;
+    }
+
+    static void DisplayResult(string userName, int squaredValue)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredValue}");
     }
 }
