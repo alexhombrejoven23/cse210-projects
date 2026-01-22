d1c95078f1cb381d317124758be6081b03c97966
Alex <alexcondorimolle2025@gmail.com>
2026-01-22 00:30:12 -0400

    my first commit

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();       
+        Fraction f2 = new Fraction(6);      
+        Fraction f3 = new Fraction(6, 7);   
+        Fraction f4 = new Fraction(1, 3);   
+
+        DisplayFraction(f1);
+        DisplayFraction(f2);
+        DisplayFraction(f3);
+        DisplayFraction(f4);
+
+        f1.SetTop(3);
+        f1.SetBottom(4);
+        Console.WriteLine("\nDespués de cambiar valores:");
+        DisplayFraction(f1);
+    }
+
+    static void DisplayFraction(Fraction fraction)
+    {
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
